683924ba2c06a949e0b82802b1e9ec5f906c3dac
AustinBoom <54464435+AustinBoom@users.noreply.github.com>
2021-07-29 16:12:20 -0600

    PlacementManager optimization: make highlight place perfectly under finger rather than corner at finger.

--- app/src/main/java/com/example/formtd/PlacementManager.java
+++ app/src/main/java/com/example/formtd/PlacementManager.java
@@ -43,14 +43,24 @@ public class PlacementManager {
             //Round touch to the nearest grid point.
             for (int i = 0; i < grid[0].length - 1; i++) {      //Round x to nearest grid.
                 if (grid[0][i].x < xTouch) {
-                    currentRectangleHighlight.left = grid[0][i].x;
+                    //If touch leans slightly left, scoot over to the right one to make placement under finger
+                    if(xTouch - grid[0][i].x < grid[0][i+1].x - xTouch && i > 0)
+                        currentRectangleHighlight.left = grid[0][i-1].x;
+                    else
+                        currentRectangleHighlight.left = grid[0][i].x;
+
                     currentRectangleHighlight.right = currentRectangleHighlight.left + (tileWidth * 2);
                 }
             }
 
             for (int i = 0; i < grid.length - 1; i++) {         //Round y to nearest grid.
                 if (grid[i][0].y < yTouch) {
-                    currentRectangleHighlight.top = grid[i][0].y;
+                    //If touch leans slightly down, scoot up one to make placement under finger
+                    if(yTouch - grid[i][0].y < grid[i+1][0].y - yTouch && i > 0)
+                        currentRectangleHighlight.top = grid[i-1][0].y;
+                    else
+                        currentRectangleHighlight.top = grid[i][0].y;
+
                     currentRectangleHighlight.bottom = currentRectangleHighlight.top + (tileWidth * 2);
                 }
             }
