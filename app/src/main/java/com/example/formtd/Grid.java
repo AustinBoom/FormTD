a25f6048fb52dec549f6d17eb64a175cd587f541
AustinBoom <54464435+AustinBoom@users.noreply.github.com>
2021-07-30 14:07:03 -0600

    Pushing Grid.

---
+++ app/src/main/java/com/example/formtd/Grid.java
@@ -0,0 +1,28 @@
+package com.example.formtd;
+
+public class Grid {
+    public int x;
+    public int y;
+    public boolean available;
+
+    public Grid(){
+        this.x = 0;
+        this.y = 0;
+        this.available = false;
+    }
+
+    public Grid(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Grid(boolean available){
+        this.available = available;
+    }
+
+    public Grid(int x, int y, boolean available){
+        this.x = x;
+        this.y = y;
+        this.available = available;
+    }
+}
