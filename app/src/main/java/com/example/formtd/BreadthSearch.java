1574d2743afccb95f822c3b5bcb7242b46a17c76
AustinBoom <54464435+AustinBoom@users.noreply.github.com>
2021-08-08 13:46:43 -0600

    Added center and endpoints.

--- app/src/main/java/com/example/formtd/BreadthSearch.java
+++ app/src/main/java/com/example/formtd/BreadthSearch.java
@@ -6,20 +6,35 @@ import java.util.ArrayList;
 
 public class BreadthSearch {
     private ArrayList<Point> enemyWayPoints;
+    private Point endPoint;
+    private Point[] centerPoints;
     //pass all needed params in constructor
-    public BreadthSearch(){
+    public BreadthSearch()
+    {
+        //Note: start point is determined by enemy.
+        endPoint = new Point(DefenceView.grid[27][10].x, DefenceView.grid[27][10].y);
+        centerPoints = new Point[]{
+                new Point(DefenceView.grid[DefenceView.grid.length/2][DefenceView.grid[0].length/2 - 1].x, DefenceView.grid[DefenceView.grid.length/2 - 1][DefenceView.grid[0].length/2].y),
+                new Point(DefenceView.grid[DefenceView.grid.length/2][DefenceView.grid[0].length/2 - 1].x + DefenceView.tileWidth, DefenceView.grid[DefenceView.grid.length/2 - 1][DefenceView.grid[0].length/2].y),
+                new Point(DefenceView.grid[DefenceView.grid.length/2][DefenceView.grid[0].length/2 - 1].x, DefenceView.grid[DefenceView.grid.length/2 - 1][DefenceView.grid[0].length/2].y + DefenceView.tileWidth),
+                new Point(DefenceView.grid[DefenceView.grid.length/2][DefenceView.grid[0].length/2 - 1].x + DefenceView.tileWidth, DefenceView.grid[DefenceView.grid.length/2 - 1][DefenceView.grid[0].length/2].y + DefenceView.tileWidth)
+        };
         enemyWayPoints = new ArrayList<>();
     }
 
     //Will calculate and return the path
     public ArrayList<Point> getUpToDatePath(){
-        //Use DefenceView.grid to access grid info
-        //TODO make test path, then pass it to the wave to follow it.
+        //TODO make breadthfirst search!
         enemyWayPoints = new ArrayList<>();
+
+        //Some test points.
         enemyWayPoints.add(new Point(DefenceView.grid[3][10].x, DefenceView.grid[3][10].y));
         enemyWayPoints.add(new Point(DefenceView.grid[6][12].x, DefenceView.grid[6][12].y));
         enemyWayPoints.add(new Point(DefenceView.grid[14][4].x, DefenceView.grid[14][4].y));
-        enemyWayPoints.add(new Point(DefenceView.grid[27][10].x, DefenceView.grid[27][10].y));
+
+
+        //Add end point
+        enemyWayPoints.add(endPoint);
         return enemyWayPoints;
     }
 
